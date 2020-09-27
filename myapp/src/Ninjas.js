138ff7626328c4f30706deae8ce1486691ca2ae7
AliAzlan1 <aliazlan2002@gmail.com>
2020-09-27 20:49:30 +0500

    Lesson 15: learnt about stateless components i.e functions

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja=> {
-            return(
-                <div className="ninja" key={ ninja.id } >
-                    <div>Name: { ninja.name } </div>
-                    <div>Age: { ninja.age } </div>
-                    <div>Belt: { ninja.belt } </div>
-                    <br></br>
-                </div>
-            )
-        })
+const Ninjas = (props) => { //we can get rid of line 4 just by getting 'props' directly in 'ninjas' by declaring function argument as ({ninjas})
+    const {ninjas} = props;
+    const ninjaList = ninjas.map(ninja=> {
         return(
-            <div className="ninja-list">
-                { ninjaList }
-            </div>   
+            <div className="ninja" key={ ninja.id } >
+                <div>Name: { ninja.name } </div>
+                <div>Age: { ninja.age } </div>
+                <div>Belt: { ninja.belt } </div>
+                <br></br>
+            </div>
         )
-    }
+    })
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>   
+    )
 }
 
 export default Ninjas
