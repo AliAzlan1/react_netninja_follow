257599b3345af5d1f8434abd39868fdd0873be24
AliAzlan1 <aliazlan2002@gmail.com>
2020-09-27 23:01:10 +0500

    Lesson 17: Revisited forms, learnt more about react forms

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 class App extends React.Component {
   state = {
@@ -14,6 +15,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Welcome to the home page!</h1>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja/>
       </div>
     );
   }
