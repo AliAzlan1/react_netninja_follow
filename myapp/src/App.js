dfac726e90e12552d146afe5eb36157839524933
AliAzlan1 <aliazlan2002@gmail.com>
2020-09-27 18:00:01 +0500

    Lesson 12: learnt nesting components, nested Ninjas inside App

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Ninjas from './Ninjas'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the home page!</h1>
+      <Ninjas/>
     </div>
   );
 }
