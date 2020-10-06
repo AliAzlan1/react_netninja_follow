0f50b5573f943acf607a15d857e670cb0e76e531
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-06 13:47:33 +0500

    Lesson 26: learn how to use Link and NavLink instead of anchor tag

--- pokeytimes/src/components/Navbar.js
+++ pokeytimes/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { Link, NavLink } from 'react-router-dom';
+//link vs Navlink: Navlink has 'active' as class while link does not have
 const Navbar = () => {
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo" href="/">Pokey' Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/About">About</a></li>
-                    <li><a href="/Contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/About">About</NavLink></li>
+                    <li><NavLink to="/Contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
