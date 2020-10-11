62aec58e91854583a64e2a8b90d1faa6a61c9e5a
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-11 17:59:57 +0500

    Lesson 41: Implemented Redux on home page in lesson 40 to fetch posts and in lesson 41 implemented redux to show details of each post on post page

--- pokeytimes/src/reducers/rootReducer.js
+++ pokeytimes/src/reducers/rootReducer.js
@@ -1,5 +1,21 @@
-initState = {
-    posts: [],
+const initState = {
+    posts: [
+        {
+            "id": '1',
+            "title": "I am Sober",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            "id": '2',
+            "title": "I am focused",
+            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+            "id": '3',
+            "title": "I can control myself",
+            "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        }
+    ],
 }
 
 function rootReducer(state = initState, action){
